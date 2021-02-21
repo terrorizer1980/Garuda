4be3ee91d7a5c48b6516cf6a278a73f31c26d7e9
The Cryptonian <73107104+Cryptonian007@users.noreply.github.com>
2021-02-21 12:35:58 +0530

    bug fixes

--- setup.py
+++ setup.py
@@ -7,14 +7,22 @@ print('[2] Enter Geolocation.io API key')
 choice = int(input('[~] Enter choice :: '))
 if choice == 1:
     api = str(input('[~] Enter API key: '))
-    with open('core\\shodan.dat', 'wb') as f:
+    if os.name == 'nt':
+        s_file = 'core\\shodan.dat'
+    else:
+        s_file = 'core/shodan.dat'
+    with open(s_file, 'wb') as f:
         pickle.dump([api], f)
     f.close()
     input('[i] Saved in core/shodan.dat. Press enter to exit...')
     exit()
 else:
     gapi = str(input('Enter API key: '))
-    with open('core\\geolocation.dat', 'wb') as f:
+    if os.name == 'nt':
+        g_file = 'core\\geolocation.dat'
+    else:
+        g_file = 'core/geolocation.dat'
+    with open(g_file, 'wb') as f:
             pickle.dump([gapi], f)
     f.close()
     input('Saved in core/geolocation.dat. Press enter to exit...')
